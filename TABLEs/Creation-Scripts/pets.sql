023314a6bc9d1a38c7fb08807804aefb2e676a91
lsaperi <lsaperi>
2025-05-29 17:29:26 -0300

    sdf; TaskID: task11

--- TABLEs/Creation-Scripts/pets.sql
+++ TABLEs/Creation-Scripts/pets.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `pets` (
   `idpets` int NOT NULL,
-  `ownerid` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `ownerid` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
   `age` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idpets`)
+  PRIMARY KEY (`idpets`,`ownerid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
