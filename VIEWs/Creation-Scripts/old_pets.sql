bad0da96534a871eff510a1cf877292045a9bb82
lsaperi <lsaperi>
2025-05-29 15:14:13 -0300

    gfgdf; TaskID: tasj9

---
+++ VIEWs/Creation-Scripts/old_pets.sql
@@ -0,0 +1,8 @@
+create ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `old_pets` as 
+
+select `pets`.`idpets` as `idpets`,
+	`pets`.`ownerid` as `ownerid`,
+	`pets`.`name` as `name`,
+	`pets`.`age` as `age` 
+from `pets` 
+where (`pets`.`age` > 10)
